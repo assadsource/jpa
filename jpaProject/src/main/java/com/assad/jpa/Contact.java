48fdc02f9c8cf89047075dac2b62582727780669
assad <assadice@yahoo.com>
2013-11-22 23:16:07 +0600

    a

--- jpaProject/src/main/java/com/assad/jpa/Contact.java
+++ jpaProject/src/main/java/com/assad/jpa/Contact.java
@@ -1,5 +1,6 @@
 package com.assad.jpa;
 
-public class Contact {
-
+public final class Contact {
+	public final static String STATUS_SUCCESS = "SUCCESS";
+	public final static String STATUS_FAIL = "FAIL";
 }
